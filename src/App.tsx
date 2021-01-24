9eaca7d9dfa3ce489628ee04e06065b19b8f705e
Yasushi Ikeda <yikeda6616@gmail.com>
2021-01-25 01:42:08 +0900

    logoitem

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {TweenMax, Power3} from 'gsap';
+
+
 function App() {
+  let logoItem = useRef(null);
+  let textItem = useRef(null);
+
+  useEffect(()=> {
+    console.log(logoItem);
+    TweenMax.to(logoItem, .8, {
+      opacity: 1,
+      y: -20,
+      ease: Power3.easeOut
+    })
+    TweenMax.to(textItem, .8, {
+      opacity: 1,
+      y: -20,
+      ease: Power3.easeOut,
+      delay: .2
+    })
+
+  }, [])
+
+  console.log(logoItem);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img ref={el => (logoItem = el as any)} src={logo} className="App-logo" alt="logo" />
+        <p ref={el=>(textItem = el as any)}>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
